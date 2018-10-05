16ccf64773e471a85180b6756d83e22c2f7b25fa
David Jones <davejonesbkk@gmail.com>
2018-10-05 18:07:08 +0700

    Update wp-cli-script.sh

--- wp-cli-script.sh
+++ wp-cli-script.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-WEBDIR=/home/*/public_html/*
 
-for dir in $WEBDIR;
+#place to store backups
+BACKUPPATH=/backups
+
+
+#path to WP installs
+SITESTORE=/home
+
+
+#create array of sites based on folder name
+SITELIST=($(ls -lh $SITESTORE | awk '{print $9}'))
+
+#make sure the backup folder exists
+
+mkdir -p $BACKUPPATH
+
+#start the loop
+for SITE in ${SITELIST[@]};
 do
-        echo $(wp core check-update --path=/home/$dir/public_html --allow-root)
+        if [ "$SITE" == "0_README_BEFORE_DELETING_VIRTFS" ] ; then
+                continue;
+        fi
 
+        echo Updating in $SITE
+        #enter the Wordpress folder
+        cd $SITESTORE/$SITE/public_html
+        #backup the WP folder
+        #tar -czf $BACKUPPATH/$SITE.tar .
+        #DBs all backed up to AWS s3 so no need to backup here
+        wp plugin list --path=$SITESTORE/$SITE/public_html
+        wp plugin update --all --path=$SITESTORE/$SITE/public_html;
 done
-
-
-
-#find /home/ -type d \(  -name public_html \) -exec bash -c "cd '{}' &&  echo $(wp core check-update --allow-root)" \;
-
-#www_dir=/home/
-
-#for site_path in $( cd $www_dir ; find . -type d -name 'wp-admin' | sort -n ); do
-        echo $(wp core check-update)
-#done
 
